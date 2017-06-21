cd2f7f47bb61f1d7966eb108085cc163d1508400
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-21 16:06:29 +0800

    Hold for the user input

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const main = require('./main/main');
+const main = require('./lib/main');
 
 main();
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,15 +1,16 @@
 let sinon = require("sinon");
+let readlineSync = require("cli-interact");
+
 let main = require("../lib/main");
 
 describe('main()', () => {
 
     it('should display main menu once started', () => {
-        sinon.spy(console, 'log');
+        sinon.stub(readlineSync, 'question');
         main();
-        expect(console.log.args.join()).toBe(`1. 添加学生
+        expect(readlineSync.question.args.join()).toBe(`1. 添加学生
 2. 生成成绩单
 3. 退出
 请输入你的选择（1～3）：`);
     });
-
 });
