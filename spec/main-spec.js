6946c4c27f5621bd4bc6c0cfdcdab1ff5350c8dc
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-21 16:27:12 +0800

    Should exit the app

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -13,6 +13,7 @@ describe('main()', () => {
     });
 
     it('should display main menu once started', () => {
+        readlineSync.question.returns('3');
         main();
         expect(readlineSync.question.args.join()).toBe(`1. 添加学生
 2. 生成成绩单
@@ -20,7 +21,13 @@ describe('main()', () => {
 请输入你的选择（1～3）：`);
     });
 
-    it('should go to add student info', () => {
+    it('should exit', () => {
+        readlineSync.question.returns('3');
+        main();
+        expect(readlineSync.question.calledOnce).toBe(true);
+    });
+
+    xit('should go to add student info', () => {
         readlineSync.question.returns(1);
         main();
         expect(readlineSync.question.lastCall.args.join()).toBe("请输入学生信息（格式：姓名, 学号, 民族, 班级, 学科: 成绩, ...），按回车提交：");
