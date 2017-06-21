c6886417f7817f075c88dbaee7a89cc62a66f88b
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-21 15:25:26 +0800

    Write Spec: should display main menu once started

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,9 +1,15 @@
-let fs = require("fs");
+let sinon = require("sinon");
+let main = require("../lib/main");
 
 describe('main()', () => {
 
-    it('should pass', () => {
-      expect(fs.statSync("index.html").isFile()).toBe(true);
+    it('should display main menu once started', () => {
+        sinon.spy(console, 'log');
+        main();
+        expect(console.log.args.join()).toBe(`1. 添加学生
+2. 生成成绩单
+3. 退出
+请输入你的选择（1～3）：`);
     });
 
 });
