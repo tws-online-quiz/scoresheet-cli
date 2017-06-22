24789d77cf7e18c61e19faeceb1b1cd159956d3e
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-22 10:19:37 +0800

    Spec: should print score sheet

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -42,4 +42,19 @@ describe('main()', () => {
         main();
         expect(readlineSync.question.args.join().includes("学生王大锤的成绩被添加")).toBe(true);
     });
+
+    it('should print score sheet', () => {
+        readlineSync.question.onFirstCall().returns(Command.MENU_ADD_STUDENT_INFO);
+        readlineSync.question.onSecondCall().returns("王大锤, 001, 汉, 201701, 数学: 100, 语文: 90, 英语: 80, 编程: 70");
+        readlineSync.question.onThirdCall().returns('2');
+        readlineSync.question.returns(Command.MENU_EXIT);
+        main();
+        expect(readlineSync.question.lastCall.args.join()).toBe(`成绩单
+姓名|数学|语文|英语|编程|平均分|总分 
+========================
+王大锤|100|90|80|70|85|340
+========================
+全班总分平均数：340
+全班总分中位数：340`);
+    });
 });
