6edf827f02c137dc9f906b16058920b99c43080b
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-23 11:12:21 +0800

    Implement: print score sheet

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -3,10 +3,12 @@ let readlineSync = require("cli-interact");
 
 let main = require("../lib/main");
 let Command = require("../lib/command/command");
+let {scoreService} = require("../lib/service/score_service");
 
 describe('main()', () => {
     beforeEach(() => {
         sinon.stub(readlineSync, 'question');
+        scoreService.students = [];
     });
 
     afterEach(() => {
@@ -51,18 +53,23 @@ describe('main()', () => {
         expect(readlineSync.question.lastCall.args.join().includes("请输入你的选择（1～3）：")).toBe(true);
     });
 
-    xit('should print score sheet', () => {
-        readlineSync.question.onFirstCall().returns(Command.MENU_ADD_STUDENT_INFO);
-        readlineSync.question.onSecondCall().returns("王大锤, 001, 汉, 201701, 数学: 100, 语文: 90, 英语: 80, 编程: 70");
-        readlineSync.question.onThirdCall().returns('2');
-        readlineSync.question.returns(Command.MENU_EXIT);
+    it('should print score sheet', () => {
+        readlineSync.question
+            .onCall(0).returns(Command.MENU_ADD_STUDENT_INFO)
+            .onCall(1).returns("王大锤, 001, 汉, 201701, 数学: 100, 语文: 90, 英语: 80, 编程: 70")
+            .onCall(2).returns(Command.MENU_ADD_STUDENT_INFO)
+            .onCall(3).returns("张小花, 002, 汉, 201701, 数学: 80, 语文: 100, 英语: 80, 编程: 90")
+            .onCall(4).returns(Command.MENU_PRINT_SCORE_SHEET)
+            .onCall(5).returns('001, 002')
+            .onCall(6).returns(Command.MENU_EXIT);
         main();
         expect(readlineSync.question.lastCall.args.join()).toBe(`成绩单
 姓名|数学|语文|英语|编程|平均分|总分 
 ========================
 王大锤|100|90|80|70|85|340
+张小花|80|100|80|90|87.5|350
 ========================
-全班总分平均数：340
-全班总分中位数：340`);
+全班总分平均数：345
+全班总分中位数：345`);
     });
 });
